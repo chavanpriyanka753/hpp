abeb1af032e6e923c96a4c32e1e6a117832bb58d
chavanpriyanka753 <chavanpriyanka753@gmail.com>
2022-09-11 22:18:37 +0530

    House Price Prediction V1

---
+++ artifacts/utils.py
@@ -0,0 +1,37 @@
+import numpy as np
+import pickle
+
+class hpp():
+    def __init__(self, data):
+        self.data = data
+
+    def load_model(self):
+        with open(r'artifacts/model.pkl', 'rb') as file:
+            self.model = pickle.load(file)
+
+    def predict(self):
+
+        self.load_model()
+
+        CRIM = float(self.data['CRIM'])
+        ZN = float(self.data['ZN'])
+        INDUS = float(self.data['INDUS'])
+        CHAS = float(self.data['CHAS'])
+        NOX = float(self.data['NOX'])
+        RM = float(self.data['RM']) 
+        AGE = float(self.data['AGE'])
+        DIS = float(self.data['DIS'])
+        RAD = float(self.data['RAD'])
+        TAX = float(self.data['TAX'])
+        PTRATIO = float(self.data['PTRATIO'])
+        B = float(self.data['B'])
+        LSTAT = float(self.data['LSTAT'])
+
+        array = np.array([CRIM, ZN, INDUS, CHAS, NOX, RM, AGE, DIS, RAD, TAX, PTRATIO, B, LSTAT], ndmin = 2)
+        print(array)
+
+        res = np.around(self.model.predict(array), 2)
+        print(res)
+
+        return res
+
